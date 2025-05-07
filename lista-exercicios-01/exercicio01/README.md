1640e679eba2ac16c10c1b12bb89f4aaa0c15fd0
Bernardo Ianiak <bernardoianiak@gmail.com>
2025-05-07 19:35:28 -0300

    feat: cria pastas e exercicio01 com README

---
+++ lista-exercicios-01/exercicio01/README.md
@@ -0,0 +1,7 @@
+Este programa solicita que o usuário digite um número e informa se ele é par ou impar.
+
+Como executar:
+
+Instale o prompt-sync => npm install prompt-sync
+
+Execute o arquivo => node exercicio01.js
---
+++ lista-exercicios-01/exercicio01/exercicio01.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let numero = prompt("Digite um número para verificar se ele é par ou impar2: ");
+numero = parseInt(numero, 10);
+
+if (isNaN(numero)) {
+    console.log("Por favor, insira um número inteiro válido.");
+} else {
+    if (numero % 2 === 0) {
+        console.log("O número é par.");
+    } else {
+        console.log("O número é ímpar.");
+    }
+}2
