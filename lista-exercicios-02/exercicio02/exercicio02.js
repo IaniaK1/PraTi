726b5565a00cbbe5017d3962a74318a0586e7b30
Bernardo Ianiak <bernardoianiak@gmail.com>
2025-06-21 22:59:39 -0300

    feat: Adiciona exercicio02

---
+++ lista-exercicios-02/exercicio02/exercicio02.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+let numeroUsuario = parseInt(prompt("Digite um número entre 1 e 100: "));
+
+while (numeroUsuario !== numeroAleatorio) {
+    if (isNaN(numeroUsuario) || numeroUsuario < 1 || numeroUsuario > 100) {
+        console.log("Por favor, insira um número válido entre 1 e 100.");
+    } else if (numeroUsuario < numeroAleatorio) {
+        console.log("O número é maior que o que você digitou. Tente novamente.");
+    } else {
+        console.log("O número é menor que o que você digitou. Tente novamente.");
+    }
+    
+    numeroUsuario = parseInt(prompt("Digite um número entre 1 e 100: "));
+}
+
+console.log("Parabéns! Você acertou o número:", numeroAleatorio);
+console.log("Fim do jogo.");
